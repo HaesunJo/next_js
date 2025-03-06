4153577472b0b3094c42a07148e2ffdc242ea76e
haillie <hsj30338@gmail.com>
2025-03-06 03:46:38 -0800

    udpate styles

--- components/moive-info.tsx
+++ components/moive-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page"
+import styles from "../styles/movie-info.module.css"
 
 async function getMoive(id:string) {
 	await new Promise((resolve) => setTimeout(resolve, 1000))
@@ -11,5 +12,16 @@ async function getMoive(id:string) {
 export default async function MovieInfo({id}: {id:string}){
     const movie = await getMoive(id)
 
-    return <h6>{JSON.stringify(movie)}</h6>
+	// return <h6>{JSON.stringify(movie)}</h6>
+	return (
+		<div className={styles.container}>
+			<img src={movie.poster_path} className={styles.poster} />
+			<div className={styles.info}>
+				<h1 className={styles.title}>{movie.title}</h1>
+				<h3 className={styles.rates}>{movie.vote_average.toFixed(1)}</h3>
+				<p className={styles.overview}>{movie.overview}</p>
+				<a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+			</div>
+		</div>
+	)
 }
--- components/movie_trailers.tsx
+++ components/movie_trailers.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page"
+import styles from "../styles/movie-trailers.module.css"
 
 async function getTrailer(id:string){
 	const response = await fetch(`${API_URL}/${id}/videos`)
@@ -6,7 +7,20 @@ async function getTrailer(id:string){
 	return response.json()
 }
 export default async function MovieTrailers({id}: {id:string}){
-    const trailers = await getTrailer(id)
-    
-    return <h6>{JSON.stringify(trailers)}</h6>
+	const trailers = await getTrailer(id)
+	
+	// return <h6>{JSON.stringify(trailers)}</h6>
+	return (
+		<div className={styles.container}>
+			{trailers.map(video =>
+				<iframe
+					key={video.id}
+					title={video.name}
+					src={`https://www.youtube.com/embed/${video.key}`}
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+					allowFullScreen
+					/>
+				)}
+		</div>
+	)
 }
